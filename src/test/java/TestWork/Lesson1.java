b179e5c33d995fc283d0d389f959350a5929d8c2
MarselKh <xacangan@mail.ru>
2021-10-21 07:09:46 +0500

    GeekBrainsStudy

---
+++ src/test/java/TestWork/Lesson1.java
@@ -0,0 +1,8 @@
+package TestWork;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.print("Hello Java");
+        System.out.println("20.10.2021");
+    }
+}
